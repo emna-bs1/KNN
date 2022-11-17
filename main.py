57c4e0dcc554f86363905e4dceced358d127e0f5
emna_bs1 <53300916+emna-bs1@users.noreply.github.com>
2022-11-17 18:24:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ max_col = []
 normalized = []
 counter = 0
 
+"""
+    functions
+"""
 
+# checks if a given input is a number
 def check_if_number(element):
     try:
         float(element)
@@ -17,44 +21,17 @@ def check_if_number(element):
         return False
 
 
-with open('Heart_Disease.csv', encoding='utf-8-sig') as heart_info:
-    reader = csv.reader(heart_info, delimiter=',')
-    columns = list(zip(*reader))
-
-    for col_ind in range(len(columns) - 1):
-        filtered = list(filter(check_if_number, columns[col_ind]))
-        if len(filtered) != len(columns[col_ind]):
-            print(columns[col_ind])
-            filtered = list(map(lambda x: float(x), filtered))
-            columns[col_ind] = tuple(','.join(columns[col_ind]).replace('?', str(mean(filtered))).split(','))
-        columns[col_ind] = tuple(map(lambda x: float(x), columns[col_ind]))
-
-        min_col.append(min(columns[col_ind]))
-        max_col.append(max(columns[col_ind]))
-
-        res = [*columns[col_ind]]
-        denominator = max_col[col_ind]
-        denominator -= min_col[col_ind]
-
-        normalized.append(tuple(map(lambda x: (x - min_col[col_ind]) / denominator, res)))
-    normalized.append(columns[len(columns) - 1])
-
-print(min_col)
-print(max_col)
-print(normalized)
-
-
+# divide data included in the file heart.csv into a training set and a test set
 def split_list(to_split):
+    # training set is 70% of available data
     training_percentage = int(len(to_split) * 0.7)
     print(training_percentage)
+    # shuffle the list
     random.shuffle(to_split)
     return to_split[:training_percentage], to_split[training_percentage:]
 
 
-training_set, test_set = split_list(list(map(tuple, zip(*normalized))))
-print(training_set, test_set)
-
-
+#
 def k_min(distances, k_val):
     k_mins_target = []
     dis = [elem[0] for elem in distances]
@@ -65,6 +42,7 @@ def k_min(distances, k_val):
     return k_mins_target
 
 
+# KNN algorithm
 def result(t):
     elements = set(t)
     occ = dict()
@@ -77,11 +55,13 @@ def result(t):
     return values
 
 
+# knn algorithm
 def knn(training_s, distance_type, k_val, test):
     distances = []
     for row_ind in range(len(training_s)):
         distance_calc = 0
         for col_index in range(len(test) - 1):
+            # Manhattan distance
             if distance_type == 1:
                 distance_calc += abs(training_s[row_ind][col_index] - test[col_index])
             else:
@@ -91,7 +71,7 @@ def knn(training_s, distance_type, k_val, test):
         distances.append((distance_calc, training_s[row_ind][len(test) - 1]))
 
     print(len(distances))
-    print(distances)
+    print("distances", distances)
     target = k_min(distances, k_val)
     print(target)
 
@@ -104,11 +84,50 @@ def knn(training_s, distance_type, k_val, test):
     print("result has to be: ", test[len(test) - 1])
 
 
+# beginning of the program
+with open('Heart_Disease.csv', encoding='utf-8-sig') as heart_info:
+    reader = csv.reader(heart_info, delimiter=',')
+    # list of columns
+    columns = list(zip(*reader))
+    print("columns:", columns)
+
+    # data cleaning (replacing unknown values with mean of the column)
+    for col_ind in range(len(columns) - 1):
+        # make sure that the list contains only elements of type number
+        # getting rid of non-numerical values
+        filtered = list(filter(check_if_number, columns[col_ind]))
+        # if true -> data set contains missing values (?)
+        if len(filtered) != len(columns[col_ind]):
+            filtered = list(map(lambda x: float(x), filtered))
+            # replace unknown values labeled as ? with the mean of the corresponding column
+            columns[col_ind] = tuple(','.join(columns[col_ind]).replace('?', str(mean(filtered))).split(','))
+            # columns contains the numerical values corresponding to the given data set
+        columns[col_ind] = tuple(map(lambda x: float(x), columns[col_ind]))
+
+        # normalizing the values (values between 0 and 1 using the mix and max following this rule : (x - mix) / (max - min))
+        min_col.append(min(columns[col_ind]))
+        max_col.append(max(columns[col_ind]))
+
+        res = [*columns[col_ind]]
+        denominator = max_col[col_ind]
+        denominator -= min_col[col_ind]
+
+        # normalized is the array that contains the data set after normalization of the values
+        normalized.append(tuple(map(lambda x: (x - min_col[col_ind]) / denominator, res)))
+        # adding the results column to the data set
+    normalized.append(columns[len(columns) - 1])
+
+
+training_set, test_set = split_list(list(map(tuple, zip(*normalized))))
+print("training set: ", training_set, 'test set: ', test_set)
+
+
 while 1:
     print("----- KNN -----")
 
     print("test set: ")
     pick_element = random.choice(test_set)
+    print("picked element: ", pick_element)
     for (i, item) in enumerate(test_set, start=1):
         print(i, item)
 
